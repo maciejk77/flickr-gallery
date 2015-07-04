b2c18057a4d7af1283b980dd001d8930d3b96b3c
Maciej Kurek <maciejk77@gmail.com>
2015-07-04 13:14:42 +0200

    Flickr photo gallery, generated on user query

--- js/flickr.js
+++ js/flickr.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-  $('button').click(function() {
-    $('button').removeClass("selected");
-    $(this).addClass("selected");
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var $searchField = $('#search');
+    
     var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
+    var animal = $searchField.val();
     var flickerOptions = {
       tags: animal,
       format: "json"
     };
-    function displayPhotos(data){
+
+    function displayPhotos(data) {
       var photoHTML = '<ul>';
       $.each(data.items, function(i, photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href=" " class="image">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       });
